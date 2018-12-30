2a29e167438b06b531a192e39048e5f506f2ce72
zatla70 <44783807+zatla70@users.noreply.github.com>
2018-12-30 21:53:29 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,8 +15,8 @@ client4.on('ready', () => {var x = client4.channels.get(process.env.ROOM_ID4);if
 
 client5.on('ready', () => {var x = client5.channels.get(process.env.ROOM_ID5);if (x) x.join();});
 
-client1.login(process.env.BOT_TOKEN);
-client2.login(process.env.BOT_TOKEN);
-client3.login(process.env.BOT_TOKEN);
-client4.login(process.env.BOT_TOKEN);
-client5.login(process.env.BOT_TOKEN);
+client1.login(process.env.BOT_TOKEN1);
+client2.login(process.env.BOT_TOKEN2);
+client3.login(process.env.BOT_TOKEN3);
+client4.login(process.env.BOT_TOKEN4);
+client5.login(process.env.BOT_TOKEN5);
