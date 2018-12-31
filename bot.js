a655a444da1bb062e8efc149a34469ce6454ea34
zatla70 <44783807+zatla70@users.noreply.github.com>
2018-12-31 15:21:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js');
 const client1 = new Discord.Client();
-const client2= new Discord.Client();
+const client2 = new Discord.Client();
 const client3 = new Discord.Client();
 const client4 = new Discord.Client();
 const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
 
 client1.on('ready', () => {var x = client1.channels.get(process.env.ROOM_ID1);if (x) x.join();});
 
@@ -15,8 +20,23 @@ client4.on('ready', () => {var x = client4.channels.get(process.env.ROOM_ID4);if
 
 client5.on('ready', () => {var x = client5.channels.get(process.env.ROOM_ID5);if (x) x.join();});
 
+client6.on('ready', () => {var x = client6.channels.get(process.env.ROOM_ID6);if (x) x.join();});
+
+client7.on('ready', () => {var x = client7.channels.get(process.env.ROOM_ID7);if (x) x.join();});
+
+client8.on('ready', () => {var x = client8.channels.get(process.env.ROOM_ID8);if (x) x.join();});
+
+client9.on('ready', () => {var x = client9.channels.get(process.env.ROOM_ID9);if (x) x.join();});
+
+client10.on('ready', () => {var x = client10.channels.get(process.env.ROOM_ID10);if (x) x.join();});
+
 client1.login(process.env.BOT_TOKEN1);
 client2.login(process.env.BOT_TOKEN2);
 client3.login(process.env.BOT_TOKEN3);
 client4.login(process.env.BOT_TOKEN4);
 client5.login(process.env.BOT_TOKEN5);
+client6.login(process.env.BOT_TOKEN6);
+client7.login(process.env.BOT_TOKEN7);
+client8.login(process.env.BOT_TOKEN8);
+client9.login(process.env.BOT_TOKEN9);
+client10.login(process.env.BOT_TOKEN10);
